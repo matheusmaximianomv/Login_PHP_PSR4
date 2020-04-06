4deb412c07caef78a1421dee14fc6129140ef5da
matheusmaximianomv <matheusmaximianomv@aluno.fapce.edu.br>
2020-04-06 11:34:40 -0300

    Adicionado as constantes de sessão

--- app/config/constants.php
+++ app/config/constants.php
@@ -1,10 +1,14 @@
 <?php
 
-/* Global Constants */
+/* Global Constants PATH */
 define("BASE_URL", "http://localhost/atividade_login");
 define("PATH_APP", dirname(__DIR__));
 define("PATH_VIEW", PATH_APP."/views/");
 define("PATH_ASSETS", PATH_APP."/assets/");
+
+/* Global Constants Session */
+define("SESSION_NAME", "user");
+define("LAST_ACCESS", "last_access");
 
 /* User Constants */
 define("USER_EMAIL", "matheus@email.com");
