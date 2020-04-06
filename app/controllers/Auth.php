9362cac841472fbc6124e225bca3231684498b8b
matheusmaximianomv <matheusmaximianomv@aluno.fapce.edu.br>
2020-04-06 15:02:34 -0300

    Refatorando a classe, extendendo métodos da super classe, adicionando as mensagens de erros e renomeando os métodos

--- app/controllers/Auth.php
+++ app/controllers/Auth.php
@@ -1,30 +1,53 @@
 <?php
 namespace App\Controllers;
 
-use App\Controllers\Pages;
+use App\Controllers\ControllerCore;
 use App\Models\User;
 
-class Auth {
-    
-    public function login() {
-        $email = $_POST["email"] ?? null;
-        $senha = $_POST["password"] ?? null;
-        
-        if($email == USER_EMAIL && $senha == USER_PASS) {
-            
-            $user = new User("Matheus Maximiano", "Graduando no Curso de Sistemas de Informação na Faculdade Paraíso, cursando o 5º Semestre.", "https://avatars1.githubusercontent.com/u/44318510?s=460&u=7ebfbdbd878a9cdbb758a36fb81ce4eb919d0f73&v=4",$email, "matheusmaximianomv", "03/03/1999", "Desenvolvedor FullStack", "(88) 9 8873-9005", "Juazeiro do Norte", "\"Esse tem sido um dos meus mantras – foco e simplicidade. Simples pode ser mais difícil de fazer do que complexo; você tem que trabalhar duro para clarear seu pensamento a fim de torná-lo simples.\" - Steve Jobs.");
+class Auth extends ControllerCore{
 
-            $_SESSION['user'] = serialize($user);
-            
-            header("Location:" . BASE_URL . "/profile");
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function signIn() {
+        if ($this->isLogged()) {
+            header('Location:'.BASE_URL);
         } else {
-            header("Location:" . BASE_URL . "/");
+            if (!empty($_POST['email']) && !empty($_POST['password'])) {
+                $email = $_POST['email'];
+                $password = $_POST['password'];
+
+                if ($email == USER_EMAIL && $password == USER_PASS) {
+
+                    $user = new User("Matheus Maximiano", "Graduando no Curso de Sistemas de Informação na Faculdade Paraíso, cursando o 5º Semestre.", $email);
+
+                    $user->setUrl_image("https://avatars1.githubusercontent.com/u/44318510?s=460&u=7ebfbdbd878a9cdbb758a36fb81ce4eb919d0f73&v=4");
+                    $user->setGithub("matheusmaximianomv");
+                    $user->setDt_birth("03/03/1999");
+                    $user->setOffice("Desenvolvedor FullStack");
+                    $user->setPhone("(88) 9 8873-9005");
+                    $user->setCity("Juazeiro do Norte");
+                    $user->setBio("\"Esse tem sido um dos meus mantras – foco e simplicidade. Simples pode ser mais difícil de fazer do que complexo; você tem que trabalhar duro para clarear seu pensamento a fim de torná-lo simples.\" - Steve Jobs.");
+
+                    $this->login($user);
+                    
+                    header("Location:" . BASE_URL . "/profile");
+                    return;
+                } else {
+                    $_SESSION[ERROR] = "Credenciais Inválidas";
+                    header('Location:'.BASE_URL);
+                }
+            } else {
+                $_SESSION[ERROR] = "Todos os campos são obrigatórios";
+                header('Location:'.BASE_URL);
+            }
         }
         
     }
 
-    public function logout() {
-        session_destroy();
+    public function destroy() {
+        $this->logout();
         header("Location:" . BASE_URL . "/");
     }
 }
